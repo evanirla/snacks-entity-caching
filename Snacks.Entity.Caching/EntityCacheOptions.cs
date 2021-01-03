835a7761fd8d39725ffa848adf560c98ad81343b
Evan Irla <ageekstouch@gmail.com>
2021-01-02 19:19:53 -0700

    Initial code Commit

---
+++ Snacks.Entity.Caching/EntityCacheOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace Snacks.Entity.Caching
+{
+    /// <summary>
+    /// Configuration for EntityCacheService
+    /// </summary>
+    public class EntityCacheOptions
+    {
+        /// <summary>
+        /// The action executed for every entry into the distributed cache.
+        /// </summary>
+        public Action<DistributedCacheEntryOptions> EntryAction { get; set; }
+    }
+}
